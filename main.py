74a395ae21f0ae2d8f26aa2bfdf963c6a4b55943
charlesxu86 <charlesxu86@163.com>
2018-08-08 15:35:49 +0800

    chatbot入口函数

---
+++ main.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 _*-  
+""" 
+@author:charlesXu
+@file: main.py 
+@desc: chatbot 入口函数
+@time: 2018/07/08 
+"""
+
+'''
+
+   Main script. See README.md for more information
+
+
+'''
+
+from Chatbot_Model import chatbot
+
+if __name__ == "__main__":
+    chatbot = chatbot.Chatbot()
+    chatbot.main()
