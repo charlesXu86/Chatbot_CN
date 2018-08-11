4a34d199c02d7e32bafa19f29e9a5d9cb8ba3ba0
charlesxu86 <charlesxu86@163.com>
2018-08-11 18:13:22 +0800

    SLU

---
+++ Chatbot_Model/SLU/basicModel.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     basicModel.py
+   Description :
+   Author :       charl
+   date：          2018/8/11
+-------------------------------------------------
+   Change Activity: 2018/8/11:
+-------------------------------------------------
+"""
+
+class KerasModel(object):
+    def __init__(self, argparams):
+        pass
--- Chatbot_Model/SLU/encoding.py
+++ Chatbot_Model/SLU/encoding.py
@@ -10,3 +10,25 @@
    Change Activity: 2018/8/11:
 -------------------------------------------------
 """
+
+import numpy as np
+
+def encoding( data, encode_type, time_length, vocab_size ):
+	if encode_type == '1hot':
+		return onehot_encoding(data, time_length, vocab_size)
+	elif encode_type == 'embedding':
+		return data
+
+def onehot_encoding( data, time_length, vocab_size):
+	X = np.zeros((len(data), time_length, vocab_size), dtype=np.bool)
+	for i, sent in enumerate(data):
+		for j, k in enumerate(sent):
+			X[i, j, k] = 1
+	return X
+
+def onehot_sent_encoding( data, vocab_size):
+	X = np.zeros((len(data), vocab_size), dtype=np.bool)
+	for i, sent in enumerate(data):
+		for j, k in enumerate(sent):
+			X[i, k] = 1
+	return X
