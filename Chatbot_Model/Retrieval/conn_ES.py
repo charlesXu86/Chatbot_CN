d3fbb675b5a00cf544d75671da3e9a35a58619a4
charlesxu86 <charlesxu86@163.com>
2018-09-04 18:23:01 +0800

    py连接es

--- Chatbot_Model/Retrieval/conn_ES.py
+++ Chatbot_Model/Retrieval/conn_ES.py
@@ -14,7 +14,19 @@
 import sys
 import json
 
+from datetime import datetime
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch([{'host':'', 'port':9200}])
+es = Elasticsearch([{'host':'test.npacn.com', 'port':9200}])
+
+# 创建索引
+es.indices.create(index='zhizhuxia')
+
+# 插入数据
+es.index(index='zhizhuxia', doc_type='test_type', id='01', body={'any': '张三是一个好人', 'timestamp': datetime.now()})
+
+# 查询数据 get 和 search 两种方式
+res = es.get(index='zhizhuxia', doc_type='test_type', id='01')
+print(res)
+print(res['_source'])
 
