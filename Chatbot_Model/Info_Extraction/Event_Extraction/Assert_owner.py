8e788ac7da3bc8907f17185800359d2baf4f461e
charlesXu86 <charlesxu86@163.com>
2018-10-17 18:38:49 +0800

    资产所有者提取   正则 + 深度学习

---
+++ Chatbot_Model/Info_Extraction/Event_Extraction/Assert_owner.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     Assert_owner.py
+   Description :  资产所有者提取   正则 + 深度学习
+   Author :       charl
+   date：          2018/10/17
+-------------------------------------------------
+   Change Activity: 2018/10/17:
+-------------------------------------------------
+"""
+
+import re
+
+def get_assert_owner(judge_res):
+    addr = ''
+    match_addr3 = re.search(r'(名下位于)', judge_res)
