4acf3b56470d9e2d1b157827b722f86d87b7f731
charlesxu86 <charlesxu86@163.com>
2018-11-01 18:08:04 +0800

    Neo4J查询处理类

--- Chatbot_KG/util/neo_models.py
+++ Chatbot_KG/util/neo_models.py
@@ -2,11 +2,11 @@
 """
 @author:charlesXu
 @file: neo_model.py
-@desc: 连接Neo4J
+@desc: Neo4J查询处理类
 @time: 2018/08/08
 """
 
-from py2neo import Graph,Node,Relationship
+from py2neo import Graph,Node,Relationship, NodeMatcher
 
 class Neo4j():
 	graph = None
@@ -14,20 +14,23 @@ class Neo4j():
 		print("create neo4j class ...")
 
 	def connectDB(self):
-		self.graph = Graph("http://localhost:7474", username="neo4j", password="123456")
+		self.graph = Graph("http://localhost:7474", username="neo4j", password="Aa123456")
 
 	def matchItembyTitle(self,value):
-		answer = self.graph.find_one(label="Item",property_key="title",property_value=value)
+		matcher = NodeMatcher(self.graph)
+		answer = matcher.match(label="Item",property_key="title",property_value=value)
 		return answer
 
 	# 根据title值返回互动百科item
 	def matchHudongItembyTitle(self,value):
-		answer = self.graph.find_one(label="HudongItem",property_key="title",property_value=value)
+		matcher = NodeMatcher(self.graph)
+		answer = matcher.match(label="HudongItem",property_key="title",property_value=value)
 		return answer
 
 	# 根据entity的名称返回关系
 	def getEntityRelationbyEntity(self,value):
-		answer = self.graph.data("MATCH (entity1) - [rel] -> (entity2)  WHERE entity1.title = \"" +value +"\" RETURN rel,entity2")
+		matcher = NodeMatcher(self.graph)
+		answer = matcher.get("MATCH (entity1) - [rel] -> (entity2)  WHERE entity1.title = \"" +value +"\" RETURN rel,entity2")
 		return answer
 
 	#查找entity1及其对应的关系（与getEntityRelationbyEntity的差别就是返回值不一样）
