d72f7556943eaade6f3c59d09dca54d35d4012dc
charlesxu86 <charlesxu86@163.com>
2018-11-02 18:10:11 +0800

    句法分析视图

---
+++ Chatbot_Web/impl/view/dp_view.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     dp_view.py
+   Description :  句法分析视图
+   Author :       charl
+   date：          2018/11/2
+-------------------------------------------------
+   Change Activity: 2018/11/2:
+-------------------------------------------------
+"""
+
+from django.shortcuts import render
+
+def dp_view(request):  # index页面需要一开始就加载的内容写在这里
+    context = {}
+    return render(request, 'dependency_parsing/dependency_parsing.html', context)
