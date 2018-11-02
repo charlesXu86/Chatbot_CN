794b4864da35761e36c41186a4ba23f9895c5301
charlesxu86 <charlesxu86@163.com>
2018-11-02 18:14:59 +0800

    中文分词视图跳转

---
+++ Chatbot_Web/impl/view/cws_view.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     cws_view.py
+   Description :   中文分词视图跳转
+   Author :       charl
+   date：          2018/11/2
+-------------------------------------------------
+   Change Activity: 2018/11/2:
+-------------------------------------------------
+"""
+
+from django.shortcuts import render
+
+def dp_view(request):  # index页面需要一开始就加载的内容写在这里
+    context = {}
+    return render(request, 'cws/cws.html', context)
