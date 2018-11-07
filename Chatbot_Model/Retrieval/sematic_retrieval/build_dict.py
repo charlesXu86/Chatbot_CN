fb7db6e0a51ebd360e4b21682e505190b0e38d0b
charlesxu86 <charlesxu86@163.com>
2018-11-07 16:14:37 +0800

    语义检索

--- Chatbot_Model/Retrieval/sematic_retrieval/build_dict.py
+++ Chatbot_Model/Retrieval/sematic_retrieval/build_dict.py
@@ -14,3 +14,79 @@
 import ahocorasick   # 字符串快速匹配
 import pickle as pkl
 
+from collections import defaultdict
+
+entity_list_file = './data/all_entity.txt'
+entity_out_path = './data/ent_ac.pkl'
+attr_list_file = './data/attr_mapping.txt'
+attr_out_path = './data/attr_ac.pkl'
+val_list_file = './data/Person_val.txt'
+
+
+def dump_ac_entity_dict(list_file, out_path):
+    '''
+
+    :param list_file:
+    :param out_path:
+    :return:
+    '''
+    A = ahocorasick.Automaton()  # AC 自动机
+    f = open(list_file)
+    i = 0
+    for line in f:
+        word = line.strip()
+        A.add_word(word, (i, word))
+        i += 1
+    A.make_automaton()
+    pkl.dump(A, open(out_path, 'wb'))
+
+def dump_ac_attr_dict(attr_mapping_file, out_path):
+    '''
+
+    :param attr_mapping_file:
+    :param out_path:
+    :return:
+    '''
+    A = ahocorasick.Automaton()
+    f = open(attr_mapping_file)
+    i = 0
+    for line in f:
+        parts = line.strip().split(" ")
+        for p in parts:
+            if p != "":
+                A.add_word(p, (i,p))
+                i += 1
+    A.make_automaton()
+    pkl.dump(A, open(out_path, 'wb'))
+
+def load_ac_dict(out_path):
+    A = pkl.load(open(out_path, 'rb'))
+    return A
+
+def load_attr_map(attr_mapping_file):
+    f = open(attr_mapping_file)
+    mapping = defaultdict(list)
+    for line in f:
+        parts = line.strip().split(" ")
+        for p in parts:
+            if p != '':
+                mapping[p].append(parts[0])
+    return mapping
+
+def load_entity_dict(entity_file):
+    f = open(entity_file)
+    ents = {}
+    for line in f:
+        ents[line.strip()] = 1
+    return ents
+
+def load_val_dict(val_file):
+    f = open(val_file)
+    val_attr_map = {}
+    for line in f:
+        parts = line.strip().split(" ")
+        val_attr_map[parts[0]] = parts[1]
+    return val_attr_map
+
+if __name__ == '__main__':
+    dump_ac_attr_dict(attr_list_file, attr_out_path)
