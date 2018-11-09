fb7cb22a534666d96b095c6d4cdbc67dbf30ae53
charlesxu86 <charlesxu86@163.com>
2018-11-09 16:21:18 +0800

    注册  登录

--- Chatbot_Web/impl/view/welcome_view.py
+++ Chatbot_Web/impl/view/welcome_view.py
@@ -11,8 +11,13 @@
 -------------------------------------------------
 """
 
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, reverse
 from Chatbot_Web.impl.login.forms import RegisterForm
+from django import forms
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
+
+from Chatbot_Web.impl.login.models import Users
 
 def welcome_view(request):  # index页面需要一开始就加载的内容写在这里
     context = {}
@@ -49,3 +54,46 @@ def register(request):
     # 如果用户通过表单提交注册信息，但是数据验证不合法，则渲染的是一个带有错误信息的表单
     # 将记录用户注册前页面的 redirect_to 传给模板，以维持 next 参数在整个注册流程中的传递
     return render(request, 'login/register.html', context={'form': form, 'next': redirect_to})
+
+# def login(request):
+#     if request.method == "POST":
+#         uf = UserFormLogin(request.POST)
+#         if uf.is_valid():
+#             # 获取表单信息
+#             username = uf.cleaned_data['username']
+#             password = uf.cleaned_data['password']
+#             userResult = Users.objects.filter(username=username, password=password)
+#             if (len(userResult)>0):
+#                 return render('success.html', {'operation':"登录"})
+#             else:
+#                 return HttpResponse("该用户不存在")
+#     else:
+#         uf = UserFormLogin()
+#     return render('login/login.html', {'uf': uf})
+
+def do_login(request):
+    if request.method == 'GET':
+        return render(request, 'login/login.html')
+
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect(reverse('base'))
+    else:
+        return render(request, 'login/login.html', {
+            'username': username,
+            'password': password
+        })
+
+class UserForm(forms.Form):
+    username = forms.CharField(label='用户名', max_length=100)
+    password1 = forms.CharField(label='密码', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='确认密码', widget=forms.PasswordInput())
+    email = forms.EmailField(label='电子邮件')
+
+class UserFormLogin(forms.Form):
+    username = forms.CharField(label='用户名', max_length=100)
+    password = forms.CharField(label='密码', widget=forms.PasswordInput())
