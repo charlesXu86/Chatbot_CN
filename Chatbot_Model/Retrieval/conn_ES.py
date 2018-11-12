de62574bec1b19c46fa50fc37ca7783f0bfa7a72
charlesxu86 <charlesxu86@163.com>
2018-11-12 16:09:21 +0800

    py连接es

--- Chatbot_Model/Retrieval/conn_ES.py
+++ Chatbot_Model/Retrieval/conn_ES.py
@@ -13,25 +13,31 @@
 
 import sys
 import json
+import datetime
 
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
 # es = Elasticsearch([{'host':'test.npacn.com', 'port':9200, 'username':'admin', 'password': 'u1PJTXyzjVqT'}])
 es = Elasticsearch(
-    ['192.168.2.139'],    # 这里可以是list形式
+    ['192.168.11.251'],    # 这里可以是list形式
    # http_auth=('admin', 'u1PJTXyzjVqT'),
     port=9200
 )
 # 创建索引
-# es.indices.create(index='zhizhuxia')
+# es.indices.create(index='chatbot')
 
 # 插入数据
-for i in range(1000):
-    es.index(index='zhizhuxia', doc_type='test_type', id=i, body={'any': '张三是一个好人' + str(i), 'timestamp': datetime.now()})
-
+# es.index(index='chatbot_cn_retrieval', doc_type='Retrieval_type')
+start = datetime.now()
+# for i in range(1000):
+# es.index(index='chatbot_cn_retrieval', doc_type='Retrieval_type', body={'any': '张三是一个好人', 'timestamp': datetime.now()})
+# end = datetime.now()
+# costtime = (end - start).seconds
 # 查询数据 get 和 search 两种方式
 # res = es.get(index='zhizhuxia', doc_type='test_type', id='01')
-# print(res)
+# print(costtime)
 # print(res['_source'])
 
+# 删除数据
+es.delete(index='chatbot_cn_retrieval', doc_type='Retrieval_type', id='AWbs4IThcGOxL98BAcTo')
