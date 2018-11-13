f76cd3f8ebf053cc0359f0cd87ea38c945037f08
charlesxu86 <charlesxu86@163.com>
2018-11-13 16:09:06 +0800

    修改方法名称

--- Chatbot_Web/impl/view/dp_view.py
+++ Chatbot_Web/impl/view/dp_view.py
@@ -13,6 +13,6 @@
 
 from django.shortcuts import render
 
-def dp_view(request):  # index页面需要一开始就加载的内容写在这里
+def dp_page(request):  # index页面需要一开始就加载的内容写在这里
     context = {}
     return render(request, 'dependency_parsing/dependency_parsing.html', context)
