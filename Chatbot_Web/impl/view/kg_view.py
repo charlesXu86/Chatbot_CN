86e5cdfc37ad30ee0bd49ac87d7bb97fe9dfb89a
charlesxu86 <charlesxu86@163.com>
2018-11-28 20:35:54 +0800

    知识图谱页面跳转

---
+++ Chatbot_Web/impl/view/kg_view.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     kg_view.py
+   Description :  知识图谱页面跳转
+   Author :       charl
+   date：          2018/11/28
+-------------------------------------------------
+   Change Activity: 2018/11/28:
+-------------------------------------------------
+"""
+
+from django.shortcuts import render
+
+def knowledge_graph_view(request):  # 知识图谱主页面跳转
+    context = {}
+    return render(request, 'knowledge_graph/knowledge_graph.html', context)
+
+def relation_search_view(request):  # 跳转到关系搜索页面
+    context = {}
+    return render(request, 'knowledge_graph/relation.html', context)
