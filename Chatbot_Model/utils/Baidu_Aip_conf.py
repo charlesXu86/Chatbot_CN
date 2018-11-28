633f59277e042a96377925217c7c26af0b6e4219
charlesxu86 <charlesxu86@163.com>
2018-11-28 15:56:07 +0800

    百度自然语言处理api调用全局配置

--- Chatbot_Model/utils/Baidu_Aip_conf.py
+++ Chatbot_Model/utils/Baidu_Aip_conf.py
@@ -3,7 +3,7 @@
 """
 -------------------------------------------------
    File Name：     Baidu_Aip_conf.py
-   Description :
+   Description :  百度自然语言处理api调用全局配置
    Author :       charl
    date：          2018/11/28
 -------------------------------------------------
@@ -17,6 +17,7 @@ from aip import AipNlp
 APP_ID = '14966163'   # APp_ID
 API_KEY = 'MAgY5KObFySpBmrHddhb17pI'
 SECRET_KEY = 'ZXGZOkpsgyyixxZeNrWqUH81KwD4EGTX '
+token = '24.c78dfdf76c97b7df0163376ee1873a8e.2592000.1545979551.282335-14966163'  # access_token
 
 # 新建一个客户端
 client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
