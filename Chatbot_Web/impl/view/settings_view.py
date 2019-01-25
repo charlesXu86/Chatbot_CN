8694a83b8719c614047646969fe4b3b2941784bc
charlesxu86 <charlesxu86@163.com>
2019-01-25 15:55:14 +0800

    系统设置页面

---
+++ Chatbot_Web/impl/view/settings_view.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     settings_view.py
+   Description :  系统设置页面
+   Author :       charl
+   date：          2019/1/25
+-------------------------------------------------
+   Change Activity: 2019/1/25:
+-------------------------------------------------
+"""
+
+from django.shortcuts import render
+
+def dialogue_page(request):
+    context = {}
+    return render(request, 'settings.html', context)
