0c9af08791bbfa025a30151c20ef123faff7dbee
charlesxu86 <charlesxu86@163.com>
2019-01-25 16:07:56 +0800

    系统说明页面

---
+++ Chatbot_Web/impl/view/specification_view.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+-------------------------------------------------
+   File Name：     specification_view.py
+   Description :  系统说明页面
+   Author :       charl
+   date：          2019/1/25
+-------------------------------------------------
+   Change Activity: 2019/1/25:
+-------------------------------------------------
+"""
+
+from django.shortcuts import render
+
+def dialogue_page(request):
+    context = {}
+    return render(request, 'specification.html', context)
