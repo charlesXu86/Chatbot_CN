93118997bb2ad8f4e6c208004a537d2752798ee3
charlesXu86 <charlesxu86@163.com>
2019-01-26 20:41:50 +0800

    settings

--- Chatbot_CN/settings.py
+++ Chatbot_CN/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'bootstrap3',
     #'rest_framework_swagger',
     'rest_framework',
-    'rest_framework_docs',
+   # 'rest_framework_docs',
     'Chatbot_Web.authdata',
 ]
 
