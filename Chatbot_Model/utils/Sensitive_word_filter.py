6d9fa623eb501d4374b76a97f44ee75538202e37
charlesXu86 <charlesxu86@163.com>
2019-06-06 17:03:37 +0800

    敏感词过滤工具类

---
+++ Chatbot_Model/utils/Sensitive_word_filter.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+
+'''
+@Author  :   Xu
+ 
+@Software:   PyCharm
+ 
+@File    :   Sensitive_word_filter.py
+ 
+@Time    :   2019-06-06 11:28
+ 
+@Desc    :   敏感词过滤接口
+ 
+'''
+
+from collections import defaultdict
+import re
+
+class NaiveFilter():
+
+    '''Filter Messages from keywords
+
+    very simple filter implementation
+
+    '''
+
+    def __init__(self):
+        self.keywords = set([])
+
+    def parse(self, path):
+        for keyword in open(path):
+            self.keywords.add(keyword.strip().decode('utf-8').lower())
+
+    def filter(self, message, repl="*"):
+        message = message.lower()
+        for kw in self.keywords:
+            message = message.replace(kw, repl)
+        return message
+
+
+class BSFilter:
+
+    '''Filter Messages from keywords
+
+    Use Back Sorted Mapping to reduce replacement times
+
+    '''
+
+    def __init__(self):
+        self.keywords = []
+        self.kwsets = set([])
+        self.bsdict = defaultdict(set)
+        self.pat_en = re.compile(r'^[0-9a-zA-Z]+$')  # english phrase or not
+
+    def add(self, keyword):
+        if not isinstance(keyword):
+            keyword = keyword.decode('utf-8')
+        keyword = keyword.lower()
+        if keyword not in self.kwsets:
+            self.keywords.append(keyword)
+            self.kwsets.add(keyword)
+            index = len(self.keywords) - 1
+            for word in keyword.split():
+                if self.pat_en.search(word):
+                    self.bsdict[word].add(index)
+                else:
+                    for char in word:
+                        self.bsdict[char].add(index)
+
+    def parse(self, path):
+        with open(path, "r") as f:
+            for keyword in f:
+                self.add(keyword.strip())
+
+    def filter(self, message, repl="*"):
+        if not isinstance(message):
+            message = message.decode('utf-8')
+        message = message.lower()
+        for word in message.split():
+            if self.pat_en.search(word):
+                for index in self.bsdict[word]:
+                    message = message.replace(self.keywords[index], repl)
+            else:
+                for char in word:
+                    for index in self.bsdict[char]:
+                        message = message.replace(self.keywords[index], repl)
+        return message
+
+
+class DFAFilter():
+
+    '''Filter Messages from keywords
+
+    Use DFA to keep algorithm perform constantly
+
+    '''
+
+    def __init__(self):
+        self.keyword_chains = {}
+        self.delimit = '\x00'
+
+    def add(self, keyword):
+        keyword = keyword.lower()
+        chars = keyword.strip()
+        if not chars:
+            return
+        level = self.keyword_chains
+        for i in range(len(chars)):
+            if chars[i] in level:
+                level = level[chars[i]]
+            else:
+                if not isinstance(level, dict):
+                    break
+                for j in range(i, len(chars)):
+                    level[chars[j]] = {}
+                    last_level, last_char = level, chars[j]
+                    level = level[chars[j]]
+                last_level[last_char] = {self.delimit: 0}
+                break
+        if i == len(chars) - 1:
+            level[self.delimit] = 0
+
+    def parse(self, path):
+        data = '/Users/charlesxu/PycharmProjects/Chatbot_CN/Chatbot_Data/Sensitive_word/keywords'
+        with open(data) as f:
+            for keyword in f:
+                self.add(keyword.strip())
+
+    def filter(self, message, repl="*"):
+        message = message.lower()
+        ret = []
+        start = 0
+        while start < len(message):
+            level = self.keyword_chains
+            step_ins = 0
+            for char in message[start:]:
+                if char in level:
+                    step_ins += 1
+                    if self.delimit not in level[char]:
+                        level = level[char]
+                    else:
+                        ret.append(repl * step_ins)
+                        start += step_ins - 1
+                        break
+                else:
+                    ret.append(message[start])
+                    break
+            else:
+                ret.append(message[start])
+            start += 1
+
+        return ''.join(ret)
+
+
+if __name__ == "__main__":
+    # gfw = NaiveFilter()
+    # gfw = BSFilter()
+
+    data = '/Users/charlesxu/PycharmProjects/Chatbot_CN/Chatbot_Data/Sensitive_word/keywords'
+    gfw = DFAFilter()
+    gfw.parse(data)
+    import time
+    t = time.time()
+    print(gfw.filter("最近有人在宣传法轮功"))
+    print(time.time() - t)
+
